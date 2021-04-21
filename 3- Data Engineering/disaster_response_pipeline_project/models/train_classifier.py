9250bc3e84263a046f697178047228b1cc90a38a
GerasimovskaMelanija <melanija_gerasimovska.com>
2021-04-21 21:54:15 +0200

    Added train model

--- 3- Data Engineering/disaster_response_pipeline_project/models/train_classifier.py
+++ 3- Data Engineering/disaster_response_pipeline_project/models/train_classifier.py
@@ -1,25 +1,96 @@
 import sys
-
+# import libraries
+import re
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.model_selection import GridSearchCV
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sqlalchemy import create_engine
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+import nltk
+nltk.download(['punkt','wordnet'])
 
 def load_data(database_filepath):
-    pass
-
+    """This function loads the dataset from database"""
+    engine = create_engine(database_filepath)
+    df = pd.read_sql_table('Disaster_Messages', con=engine)
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    return X,Y
 
 def tokenize(text):
-    pass
+    """This function tokenize the text"""
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # initiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+
+    # iterate through each token
+    clean_tokens = []
+    for tok in tokens:
+        
+        # lemmatize, normalize case, and remove leading/trailing white space
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """This function returns machine learning pipeline"""
+    pipeline = Pipeline([
+            
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+    
+        ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+    ])
+
+    # specify parameters for grid search
+    parameters = { 'features__text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+                  'clf__n_neighbors'  : [5, 15],
+                  'clf__weights' : ['uniform', 'distance'],
+
+    }
+
+    # create grid search object
+    cv = GridSearchCV(pipeline, param_grid = parameters)
+    
+    return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+def evaluate_model(model, X_test, y_test):
+    """This function predicts X_test and display the information about the predicted value and the real value"""
+    y_pred = model.predict(X_test)
+    for i, col in enumerate(y_test):
+        print('For Column: ', col)
+        print(classification_report(y_test.values[i], y_pred[i]))
+        
+    for i, col in enumerate(y_test):
+        print('F1_score for Column: ', col)
+        print(f1_score(y_test.values[i], y_pred[i]))
+        
+    print(accuracy = (y_pred == y_test).mean())
+       
 
 def save_model(model, model_filepath):
-    pass
-
+    """This function saves the model as pickle file"""
+    with open (model_filepath, 'wb') as pickle_file:
+        pickle.dump(model, pickle_file)
 
 def main():
     if len(sys.argv) == 3:
@@ -35,7 +106,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
